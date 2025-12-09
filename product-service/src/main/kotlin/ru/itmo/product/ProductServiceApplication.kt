3a3d45f6c14a75bac22e1aa16a88996ad93cfe89
Pleanar <113443019+Pleanar@users.noreply.github.com>
2025-12-09 16:52:05 +0300

    fix: fixed annotations for ProductServiceApplication

--- product-service/src/main/kotlin/ru/itmo/product/ProductServiceApplication.kt
+++ product-service/src/main/kotlin/ru/itmo/product/ProductServiceApplication.kt
@@ -2,9 +2,12 @@ package ru.itmo.product
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
 class ProductServiceApplication
 
 fun main(args: Array<String>) {
